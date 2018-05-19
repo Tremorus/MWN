183f4513b973cbd95cf67b189949bbe31fff144e
Андрей Милованов <tremoridze@gmail.com>
2018-05-19 21:28:34 +0300

    initial commit

---
+++ My Web Notes/MWN/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MWN.Models
+{
+    public class Note
+    {
+        public string Id { get; set; }
+        public string Owner { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public Note()
+        {
+
+        }
+    }
+}
